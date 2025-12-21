70ef9353a8f18cf4d32fb199d952987253effbc8
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-12-21 17:39:51 +0700

    updated

--- robot_move/robot_move/get_img.py
+++ robot_move/robot_move/get_img.py
@@ -5,7 +5,6 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
 from std_msgs.msg import String, Float32
 from cv_bridge import CvBridge
 
@@ -13,67 +12,51 @@ from cv_bridge import CvBridge
 class Worker(Node):
     def __init__(self):
         super().__init__('worker_node')
+
         pkg_path = get_package_share_directory('robot_move')
         mask_path = os.path.join(pkg_path, 'resources', 'masks', 'mask.png')
-        print(mask_path)
         mask_ref = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-        print(mask_ref)
         _, self.soft_mask_ref = cv2.threshold(mask_ref, 127, 255, cv2.THRESH_BINARY)
 
         self.bridge = CvBridge()
 
-        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.finish_publisher_ = self.create_publisher(String, "/robot_finish", 10)
-        self.aruco_publisher_ = self.create_publisher(Float32, "/mission_aruco", 10)
+        self.team_aruco_publisher = self.create_publisher(Float32, "/team/aruco", 10)
+        self.team_arrow_publisher = self.create_publisher(String, "/team/arrow", 10)
+        self.team_start_publisher = self.create_publisher(String, "/team/start", 10)
 
+        self.commands_sub = self.create_subscription(Image, '/team/commands', self.commands_callback, 10)
         self.color_sub = self.create_subscription(Image, '/color/image', self.color_callback, 10)
         self.depth_sub = self.create_subscription(Image, '/depth/image', self.depth_callback, 10)
         self.depth_map = None
         self.img = None
         self.img_hsv = None
 
-        self.aruco_detected = False
-
         self.timer = self.create_timer(0.1, self.loop)
-        self.move = False
-        self.time_start = -1
-        self.time_stop = 2.0
+        self.detect_aruco = False
+        self.detect_start = False
+        self.detect_arrow = False
 
     def loop(self):
-        green_mask = self.get_green()
+        if self.detect_start:
+            green_mask = self.get_green()
+            if green_mask is not None and np.count_nonzero(green_mask) > 1000:
+                start_msg = String()
+                start_msg.data = "start"
+                self.team_start_publisher.publish(start_msg)
 
-        if self.time_start < 0 and green_mask is not None and np.count_nonzero(green_mask) > 1000:
-            self.move = True
-
-        msg = Twist()
-        msg.linear.x = 0.0
-        msg.angular.z = 0.0
-
-        if self.move and self.time_start < self.time_stop:
-            msg.linear.x = 0.2
-            self.time_start += 0.1
-        elif self.move and self.time_start >= self.time_stop:
-            self.move = False
-            # self.finish()
-
-        if not self.aruco_detected:
+        if self.detect_aruco:
             aruco = self.check_aruco()
             if aruco is not None:
-                ac_msg = Float32()
-                ac_msg.data = aruco
-                self.aruco_publisher_.publish(ac_msg)
-                self.aruco_detected = True
+                aruco_msg = Float32()
+                aruco_msg.data = aruco
+                self.team_aruco_publisher.publish(aruco_msg)
 
-        sign = self.get_sign()
-        print(sign)
-
-        self.publisher_.publish(msg)
-
-    def finish(self):
-        msg = String()
-        msg.data = "test"
-
-        self.finish_publisher_.publish(msg)
+        if self.detect_arrow:
+            sign = self.get_sign()
+            if sign is not None:
+                sign_msg = String()
+                sign_msg.data = sign[0]
+                self.team_arrow_publisher.publish(sign_msg)
 
     def check_aruco(self):
         if self.img is None:
@@ -88,12 +71,9 @@ class Worker(Node):
 
         if ids is not None:
             cv2.aruco.drawDetectedMarkers(self.img, corners, ids)
+            res = np.sqrt(ids.flatten()[0])
+            return res
 
-            print(f"Найдено маркеров: {len(ids)}")
-            print(f"ID маркеров: {ids.flatten()}")
-            print("Res:", np.sqrt(ids.flatten()[0]))
-
-            return np.sqrt(ids.flatten()[0])
         return None
     
     def get_sign(self):
@@ -132,13 +112,6 @@ class Worker(Node):
             return mask
         return None
 
-    def get_yellow(self):
-        if self.img_hsv is not None:        
-            mask = (self.img_hsv[..., 1] >= 0.8)
-            mask &= (self.img_hsv[..., 0] >= np.deg2rad(50)) & (self.img_hsv[..., 0] <= np.deg2rad(70))
-            return mask
-        return None
-
     def color_callback(self, msg):
         self.img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         self.img_hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV).astype(np.float32)
@@ -157,9 +130,12 @@ class Worker(Node):
         if np.any(mask):
             depth_data[mask] = cv_depth[mask]
             self.depth_map = depth_data
-            np.save("depth.npy", self.depth_map)
+            # np.save("depth.npy", self.depth_map)
         else:
             self.depth_map = None
+
+    def commands_callback(self, msg):
+        com = msg.data
 
 
 def main(args=None):
